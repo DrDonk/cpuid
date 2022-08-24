25e572b90226112d183de803b87c74252b662b94
David Parsons <dave@daveparsons.net>
2022-08-24 13:59:17 +0100

    build: Add generated license.h and build.h

---
+++ build.h
@@ -0,0 +1,15 @@
+#ifndef __included_cpuid_build_h
+#define __included_cpuid_build_h
+
+#define CPUID_VERSION_MAJOR 1
+#define CPUID_VERSION_MINOR 8
+#define CPUID_VERSION_REVISION 2
+#define CPUID_VERSION_BUILD 8
+#define CPUID_VERSION_TAG "1.8.2"
+#define CPUID_VERSION_LONG "1.8.2-8-g56c224a"
+
+#define CPUID_RESOURCE_VERSION 1,8,2,8
+#define CPUID_RESOURCE_VERSION_STRING "1, 8, 2, 8"
+
+#endif
+
