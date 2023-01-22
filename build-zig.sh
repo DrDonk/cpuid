99a3361c3f4165b4d79e6b9f51e20e5d44db92b4
David Parsons <dave@daveparsons.net>
2023-01-22 21:22:08 +0000

    cpuid: Add -Wno-unused-command-line-argument to build script and clean up with CC.

--- build-zig.sh
+++ build-zig.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env zsh
 #set -x
-export CC="zig cc"
-make clean
-make
+export CC="zig cc -Wno-unused-command-line-argument"
+make CC="$CC" clean
+make CC="$CC"
 mv -v cpuid ./bin/darwin/cpuid
-make clean
-export CC="zig cc -target x86_64-linux-musl"
+make CC="$CC" clean
+export CC="zig cc -Wno-unused-command-line-argument -target x86_64-linux-musl"
 make CC="$CC"
 mv -v cpuid ./bin/linux/cpuid
-make clean
-export CC="zig cc -target x86_64-windows-gnu"
+make CC="$CC" clean
+export CC="zig cc -Wno-unused-command-line-argument -target x86_64-windows-gnu"
 make CC="$CC"
 mv -v cpuid ./bin/windows/cpuid.exe
+make CC="$CC" clean
+export CC="zig cc -Wno-unused-command-line-argument"
 make clean
-export CC="zig cc"
